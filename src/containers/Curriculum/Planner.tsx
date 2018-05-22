8ec660a647f2c3ac3416c90ce5dddc08c36f24c5
parkchansoo <chansoo1563@gmail.com>
2018-05-22 20:00:19 +0900

    initail setting

--- src/containers/Curriculum/Planner.tsx
+++ src/containers/Curriculum/Planner.tsx
@@ -1,48 +1,15 @@
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router-dom';
 
-import Board, { IBoardData } from 'react-trello';
+import Board from 'react-trello';
 
-const dummyData: IBoardData = {
-  lanes: [
-    {
-      id: 'lane1',
-      label: 'lane label',
-      title: 'Hello',
-
-      cards: [
-        {
-          id: '123',
-          label: 'this is label',
-          title: 'world',
-
-          description: 'this is description',
-        },
-      ],
-    },
-    {
-      id: 'lane2',
-      label: 'lane label',
-      title: 'Lane 2',
-
-      cards: [
-        {
-          id: '123',
-          label: 'this is label',
-          title: 'world',
-
-          description: 'this is description',
-        },
-      ],
-    },
-  ],
-};
+import { semesterBoards } from '@src/constants';
 
 export default class Planner extends React.Component<RouteComponentProps<{}>> {
   public render() {
     return (
       <div>
-        <Board draggable data={dummyData} />
+        <Board draggable editable data={semesterBoards} />
       </div>
     );
   }
