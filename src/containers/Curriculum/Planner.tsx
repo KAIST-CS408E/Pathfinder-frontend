176e81ab07a05e9b53e875a62ce1e59a7f87b34a
hyperthreading <no1expman@gmail.com>
2018-05-25 19:47:37 +0900

    Add pinned lane to kanban

--- src/containers/Curriculum/Planner.tsx
+++ src/containers/Curriculum/Planner.tsx
@@ -1,25 +1,25 @@
-import * as React from "react";
-import { connect, Dispatch } from "react-redux";
-import { bindActionCreators } from "redux";
+import * as React from 'react';
+import { connect, Dispatch } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
-import { Container, Draggable } from "react-smooth-dnd";
+import { Container, Draggable } from 'react-smooth-dnd';
 
-import { plannerBoardData } from "@src/constants";
-import { RootState } from "@src/redux";
-import { actions as plannerActions } from "@src/redux/planner";
+import { plannerBoardData } from '@src/constants';
+import { RootState } from '@src/redux';
+import { actions as plannerActions } from '@src/redux/planner';
 
-import { ISemester } from "pathfinder";
+import { ISemester } from 'pathfinder';
 
-import styles from "./Planner.style";
+import styles from './Planner.style';
 
-import Avatar from "@material-ui/core/Avatar";
-import Chip from "@material-ui/core/Chip";
-import Typography from "@material-ui/core/Typography";
+import Avatar from '@material-ui/core/Avatar';
+import Chip from '@material-ui/core/Chip';
+import Typography from '@material-ui/core/Typography';
 
 // icons
-import Description from "@material-ui/icons/Description";
+import Description from '@material-ui/icons/Description';
 
-import { PlayCircleOutline, ThumbUp } from "@material-ui/icons";
+import { PlayCircleOutline, ThumbUp } from '@material-ui/icons';
 
 const { classes } = styles;
 
@@ -30,7 +30,6 @@ interface IProps {
   onAddCourse: typeof plannerActions.addCourse;
   onRemoveCourse: typeof plannerActions.removeCourse;
 }
-
 
 const profColorD = '#9E9E9E';
 // const profColorS = '#536DFE';
@@ -68,6 +67,63 @@ class Planner extends React.Component<IProps> {
 
   public preventDragging = (e: React.MouseEvent<HTMLElement>) => {
     e.preventDefault();
+  };
+
+  public renderPinnedCourse() {
+    const { boardData } = this.props;
+
+    if (boardData === undefined || boardData.length === 0) {
+      return <>Loading</>;
+    }
+    const pinnedCourseLane = boardData[boardData.length - 1];
+
+    return (
+      <Container
+        key={pinnedCourseLane.id}
+        groupName="col"
+        orientation="vertical"
+        onDrop={this.onCardDrop(pinnedCourseLane.id)}
+        getChildPayload={this.getChildPayload(pinnedCourseLane.id)}
+      >
+        <header className={classes.laneHeader}>
+          <div className={classes.laneTitle}>{pinnedCourseLane.id}</div>
+          <div className={classes.laneLabel}>
+            {pinnedCourseLane.label
+              ? pinnedCourseLane.label
+              : 'Load: -.- Grade: -.-'}
+          </div>
+        </header>
+        {pinnedCourseLane.courses.map(course => {
+          return (
+            <Draggable
+              onClick={this.preventDragging}
+              key={course.id}
+              className={classes.card}
+            >
+              <header className={classes.cardHeader}>
+                <div style={{ width: '100%' }}>
+                  {course.name}
+                  <span style={{ fontSize: 12 }}>
+                    {course.label ? course.label : ''}
+                  </span>
+                </div>
+                <div style={{ height: 20 }}>
+                  <Description />
+                </div>
+              </header>
+              <div className={classes.cardMiddle}>
+                {course.description ? course.description : 'Load:-.- Grade:-.-'}
+              </div>
+              <div className={classes.cardProfs}>
+                <div style={{ backgroundColor: profColorD, color: 'white' }}>
+                  {course.lectures[0].professor}
+                </div>
+              </div>
+            </Draggable>
+          );
+        })}
+      </Container>
+    );
   }
 
   public render() {
@@ -84,85 +140,126 @@ class Planner extends React.Component<IProps> {
             My Academic Path Simulator
           </Typography>
           <Typography align="left" variant="subheading">
-            Plan my academic path, simulate, and get recommends based on real data.
-            <br/>* This system is helpful, but blind trust can be dangerous.
+            Plan my academic path, simulate, and get recommends based on real
+            data.
+            <br />* This system is helpful, but blind trust can be dangerous.
           </Typography>
         </div>
 
         <div className={classes.division}>{/* give division */}</div>
-        <div style={{ textAlign: "right", margin: "0vh 10vw 3vh 0vw" }}>
+        <div style={{ textAlign: 'right', margin: '0vh 10vw 3vh 0vw' }}>
           <Chip
-            style={{ marginRight: "1vw", backgroundColor: "#FF9800", color: "white"}}
+            style={{
+              marginRight: '1vw',
+
+              backgroundColor: '#FF9800',
+              color: 'white',
+            }}
             label="Course Recommendation"
             avatar={
-              <Avatar
-                style={{ backgroundColor: "#FF9800", color: "white"}}
-              >
+              <Avatar style={{ backgroundColor: '#FF9800', color: 'white' }}>
                 <ThumbUp />
               </Avatar>
-            }/>
+            }
+          />
           <Chip
-            style={{ marginRight: "1vw", backgroundColor: "#4CAF50", color: "white"}}
+            style={{
+              marginRight: '1vw',
+
+              backgroundColor: '#4CAF50',
+              color: 'white',
+            }}
             label="Course Simulation"
             avatar={
-              <Avatar
-                style={{ backgroundColor: "#4CAF50", color: "white"}}
-              >
+              <Avatar style={{ backgroundColor: '#4CAF50', color: 'white' }}>
                 <PlayCircleOutline />
               </Avatar>
-            }/>
+            }
+          />
         </div>
 
         {/* the kanban board */}
-      <div>
-        <div className={classes.boardContainer}>
-          {boardData.slice(0, -1).map(semester => (
-            <div className={ classes.semesterBoard }>
-              <Container
-                key={semester.id}
-                groupName="col"
-                orientation="vertical"
-                onDrop={this.onCardDrop(semester.id)}
-                getChildPayload={this.getChildPayload(semester.id)}
-              >
-                <header className={classes.laneHeader}>
-                  <div className={classes.laneTitle}>{ semester.id }</div>
-                  <div className={classes.laneLabel}>{ semester.label ? semester.label : "Load: -.- Grade: -.-" }</div>
-                </header>
-                {semester.courses.map(course => {
-                  return (
-                    <Draggable onClick={this.preventDragging} key={course.id} className={classes.card}>
-                      <header className={classes.cardHeader}>
-                        <div style={{ width: "100%" }}>{course.name}<span style={{fontSize: 12}}>{course.label ? course.label : ""}</span></div>
-                        <div style={{ height: 20 }}><Description/></div>
-                      </header>
-                      <div className={classes.cardMiddle}>{course.description ? course.description : "Load:-.- Grade:-.-"}</div>
-                      <div className={classes.cardProfs}>
-                        <div style={{ backgroundColor: profColorD, color: "white" }}>
-                          {course.lectures[0].professor}</div>
-                      </div>
-                    </Draggable>
-                  );
-                }
-                )}
-                </Container>
-              <div className={classes.feedback}>
-                {semester.feedback.map( feedback => {
-                  return (
-                    <div key={feedback.type} style={{ backgroundColor: feedback.ok ? passColor : warnColor }}>
-                      <div className={classes.feedbackTitle}>{feedback.type}
-                        {feedback.ok ? " balanced" : " error"}</div>
-                      <div className={classes.feedbackDetail}>{feedback.reason}</div>
+        <div>
+          <div className={classes.boardContainer}>
+            {boardData.slice(0, -1).map(semester => (
+              <div className={classes.semesterBoard}>
+                <Container
+                  key={semester.id}
+                  groupName="col"
+                  orientation="vertical"
+                  onDrop={this.onCardDrop(semester.id)}
+                  getChildPayload={this.getChildPayload(semester.id)}
+                >
+                  <header className={classes.laneHeader}>
+                    <div className={classes.laneTitle}>{semester.id}</div>
+                    <div className={classes.laneLabel}>
+                      {semester.label ? semester.label : 'Load: -.- Grade: -.-'}
                     </div>
-                  );
-                }
-                )}
+                  </header>
+                  {semester.courses.map(course => {
+                    return (
+                      <Draggable
+                        onClick={this.preventDragging}
+                        key={course.id}
+                        className={classes.card}
+                      >
+                        <header className={classes.cardHeader}>
+                          <div style={{ width: '100%' }}>
+                            {course.name}
+                            <span style={{ fontSize: 12 }}>
+                              {course.label ? course.label : ''}
+                            </span>
+                          </div>
+                          <div style={{ height: 20 }}>
+                            <Description />
+                          </div>
+                        </header>
+                        <div className={classes.cardMiddle}>
+                          {course.description
+                            ? course.description
+                            : 'Load:-.- Grade:-.-'}
+                        </div>
+                        <div className={classes.cardProfs}>
+                          <div
+                            style={{
+                              backgroundColor: profColorD,
+                              color: 'white',
+                            }}
+                          >
+                            {course.lectures[0].professor}
+                          </div>
+                        </div>
+                      </Draggable>
+                    );
+                  })}
+                </Container>
+                <div className={classes.feedback}>
+                  {semester.feedback.map(feedback => {
+                    return (
+                      <div
+                        key={feedback.type}
+                        style={{
+                          backgroundColor: feedback.ok ? passColor : warnColor,
+                        }}
+                      >
+                        <div className={classes.feedbackTitle}>
+                          {feedback.type}
+                          {feedback.ok ? ' balanced' : ' error'}
+                        </div>
+                        <div className={classes.feedbackDetail}>
+                          {feedback.reason}
+                        </div>
+                      </div>
+                    );
+                  })}
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+            {/* 여기가 핀해놓은 강의 리스트 있는 곳임 !!*/}
+            {this.renderPinnedCourse()}
+          </div>
         </div>
       </div>
-    </div>
     );
   }
 }
