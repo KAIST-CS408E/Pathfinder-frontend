b665b04564ad755818afe880d8d7ee46daa57859
Unknown <no1expman@gmail.com>
2018-05-28 19:42:07 +0900

    Add newly created lectures to sidebar

--- src/containers/CourseSearch/Sidebar.tsx
+++ src/containers/CourseSearch/Sidebar.tsx
@@ -14,13 +14,31 @@ import Tabs from '@material-ui/core/Tabs';
 import Equalizer from '@material-ui/icons/Equalizer';
 import Person from '@material-ui/icons/Person';
 
+import { getStatistics, StatisticsResponse } from '@src/api';
+import { buildCourseKey } from '@src/utils';
+
 import styles from './Search.style';
 
 const { classes } = styles;
 
-const value=0;
+const value = 0;
 
-export default class Sidebar extends React.Component {
+interface IState {
+  statistics: StatisticsResponse;
+}
+
+export default class Sidebar extends React.Component<{}, IState> {
+  constructor(props: any) {
+    super(props);
+    this.state = { statistics: [] };
+  }
+
+  public componentDidMount() {
+    getStatistics().then(json => {
+      this.setState({ statistics: json });
+    });
+  }
+
   public render() {
     return (
       <Card style={{ width: '100%', height: '100%' }}>
@@ -34,30 +52,69 @@ export default class Sidebar extends React.Component {
           <Tab label="statistics" icon={<Equalizer />} />
           <Tab label="relevance" icon={<Person />} />
         </Tabs>
-        <CardContent style={{padding:0}}>
-          {value === 0 &&
+        <CardContent style={{ padding: 0 }}>
+          {value === 0 && (
             <div className={classes.sideBarStepBoard}>
+              <p
+                style={{
+                  fontSize: '1.1rem',
+                  paddingLeft: 12,
+                  textAlign: 'left',
+                }}
+              >
+                New courses
+              </p>
               <Stepper orientation="vertical" activeStep={0}>
-                <Step>
-                  <StepLabel style={{ position: "relative", left:0, top: 0, }} className={classes.stepLabel} >
-                    <Chip label="CS230" style={{ textAlign:"left", height: 26, position:"absolute", left:0, top:-13}}/>
-                    <div className={classes.stepContent}>System Programming</div></StepLabel>
-                </Step>
-                <Step>
-                  <StepLabel style={{ position: "relative", left:0, top: 0, }} className={classes.stepLabel} >
-                    <Chip label="CS230" style={{ textAlign:"left", height: 26, position:"absolute", left:0, top:-13}}/>
-                    <div className={classes.stepContent}>System Programming</div></StepLabel>
-                </Step>
-                <Step>
-                  <StepLabel style={{ position: "relative", left:0, top: 0, }} className={classes.stepLabel} >
-                    <Chip label="CS230" style={{ textAlign:"left", height: 26, position:"absolute", left:0, top:-13}}/>
-                    <div className={classes.stepContent}>System Programming</div></StepLabel>
-                </Step>
+                {this.state.statistics.map(course => (
+                  <Step>
+                    <StepLabel
+                      style={{ position: 'relative', left: 0, top: 0 }}
+                      className={classes.stepLabel}
+                    >
+                      <CourseStep
+                        key={buildCourseKey(course)}
+                        courseNumber={course.courseNumber}
+                        subtitle={course.subtitle}
+                        name={course.name}
+                      />
+                    </StepLabel>
+                  </Step>
+                ))}
               </Stepper>
             </div>
-          }
+          )}
         </CardContent>
       </Card>
     );
   }
 }
+
+interface ICourseStepProps {
+  courseNumber: string;
+  subtitle: string;
+  name: string;
+}
+
+const CourseStep: React.SFC<ICourseStepProps> = ({
+  courseNumber,
+  subtitle,
+  name,
+}) => {
+  return (
+    <div>
+      <Chip
+        label={courseNumber}
+        style={{
+          height: 26,
+
+          left: 0,
+          top: -13,
+
+          position: 'absolute',
+          textAlign: 'left',
+        }}
+      />
+      <div className={classes.stepContent}>{name}</div>
+    </div>
+  );
+};
